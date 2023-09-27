6436014b4209c4e02c4fad5ed6b14d00f29a4886
Kanya-AngularMinds <kanya@angularminds.in>
2023-09-27 15:23:08 +0530

    handled responsiveness of login page

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -23,13 +23,13 @@ function Login() {
   return (
     <>
       <div className="flex flex-col items-center">
-        <div className="px-20 py-10 bg-white custom-shadow mt-28 w-[37rem]">
+        <div className="sm:px-20 px-5 py-10 bg-white custom-shadow mt-28 sm:w-[37rem]">
           {/* header */}
 
           <div className="flex flex-col items-center w-full">
             <button
               href="!#"
-              className="bg-neutral-100 relative  group text-start mb-4 h-[50px] rounded-lg w-[80%] flex items-center z-30 "
+              className="bg-neutral-100 relative  group text-start mb-4 h-[50px] rounded-lg sm:w-[80%]  w-full flex items-center z-30 "
             >
               <i className="flex items-center justify-center h-full  text-lg text-white rounded-l-lg rounded-r-sm w-[100px] fa fa-facebook-f"></i>{" "}
               <span className="w-full text-gray-500 transition-all duration-500 group-hover:text-white">
@@ -46,7 +46,7 @@ function Login() {
           </div>
           {/* form */}
           <div className="mt-8">
-            <form action="#">
+            <form onSubmit={(e) => handleSubmit(e)}>
               <label>
                 <p className="mb-2 font-medium text-zinc-600">Work email</p>
               </label>
@@ -55,6 +55,8 @@ function Login() {
                   type="text"
                   className="w-full py-4 pl-10 pr-4 border-none rounded-lg ring-white focus:ring-orange-400 bg-neutral-100 "
                   placeholder="Enter your email"
+                  name="email"
+                  onChange={(e) => handleInputChange(e)}
                 />
                 <div
                   class="absolute inset-y-0 left-0 pl-3 
@@ -69,9 +71,10 @@ function Login() {
               </label>
               <div class="relative mb-5">
                 <input
-                  type="text"
+                  type="password"
                   className="w-full py-4 pl-10 pr-4 border-none rounded-lg bg-neutral-100 focus:ring-orange-400"
                   placeholder="Enter your password"
+                  onChange={(e) => handleInputChange(e)}
                 />
                 <div
                   class="absolute inset-y-0 left-0 pl-3 
